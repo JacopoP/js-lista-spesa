8a43eb8b0d239c892ff3f671f363e79067cfae9d
Jac22 <jacopo.prudentino@gmail.com>
2022-11-14 13:25:14 +0100

    Sistemati link, completato bonus

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const shopList=["frutta", "Pasta", "Tonno", "latte", "passata"];
 const list=document.querySelector("ul");
 
+console
 let i=0;
 while(i<shopList.length && shopList.length!=0){
     list.innerHTML+=`<li>${shopList[i]}</li>`
@@ -10,3 +11,12 @@ while(i<shopList.length && shopList.length!=0){
 // for(let i=0; i<shopList.length; i++){
 //     list.innerHTML+=`<li>${shopList[i]}</li>`
 // }
+
+document.querySelector("button").addEventListener("click",
+    function(){
+        const newItem=prompt("Cosa vuoi inserire nella lista?");
+        console.log(newItem);
+        shopList.push(newItem);
+        list.innerHTML+=`<li>${newItem}</li>`
+    }
+);
