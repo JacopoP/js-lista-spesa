cb32a37cf1154737084974070bb38ab166a51b1b
Jac22 <jacopo.prudentino@gmail.com>
2022-11-14 13:56:29 +0100

    Migliorato il bonus

--- js/script.js
+++ js/script.js
@@ -1,22 +1,45 @@
 const shopList=["frutta", "Pasta", "Tonno", "latte", "passata"];
 const list=document.querySelector("ul");
+const ok=document.getElementById("confirm");
+const nItem=document.getElementById("new-item");
+const remove=document.getElementById("remove");
+const oItem=document.getElementById("old-item");
 
-console
+
+// ciclo while
 let i=0;
 while(i<shopList.length && shopList.length!=0){
     list.innerHTML+=`<li>${shopList[i]}</li>`
     i++
 }
 
-// for(let i=0; i<shopList.length; i++){
-//     list.innerHTML+=`<li>${shopList[i]}</li>`
-// }
 
-document.querySelector("button").addEventListener("click",
+
+ok.addEventListener("click",
     function(){
-        const newItem=prompt("Cosa vuoi inserire nella lista?");
-        console.log(newItem);
-        shopList.push(newItem);
-        list.innerHTML+=`<li>${newItem}</li>`
+        const newItem=nItem.value;
+        nItem.value="";
+        if(newItem!=""){
+            shopList.push(newItem);
+            list.innerHTML+=`<li>${newItem}</li>`
+        }
+    }
+);
+
+remove.addEventListener("click",
+    function(){
+        const oldItem=oItem.value;
+        oItem.value="";
+        if(oldItem!=""){
+            const index = shopList.indexOf(oldItem);
+            if (index > -1) {
+                shopList.splice(index, 1);
+            }
+            list.innerHTML="";
+            // ciclo for
+            for(let i=0; i<shopList.length; i++){
+                list.innerHTML+=`<li>${shopList[i]}</li>`
+            }
+        }
     }
 );
